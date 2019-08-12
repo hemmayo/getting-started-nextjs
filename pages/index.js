6badd3119369c812489478e1bd121043f5399921
Emmanuel Popoola <olaayo10@gmail.com>
2019-08-12 23:00:13 +0100

    navigating between pages

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
+import Link from "next/link";
+
 const Index = () => (
   <div>
+    <Link title="About Page" href="/about">
+      <a>About Page</a>
+    </Link>
     <p>Hello Next.js</p>
   </div>
 );
